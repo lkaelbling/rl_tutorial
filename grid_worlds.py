8815204d74e441887689aa102ca52174bfa2376f
lpk <lpk@csail.mit.edu>
2024-08-11 13:35:12 -0400

    Starting files

---
+++ grid_worlds.py
@@ -0,0 +1,424 @@
+import pdb
+from mdp import MDP
+from dist import DDist, uniform_dist
+import numpy as np
+import matplotlib.pyplot as plt
+
+######################################################################
+# Grid world
+######################################################################
+
+# A subclass of MDPs, defined by a simple 2D discrete grid map.  All
+# have states of the form (i, j) indicating discrete indices into the
+# map, and actions are 'n', 's', 'e', 'w'.
+
+
+# Special characters in map
+# '#' : wall, location is uninhabitable
+# '*' : +50 reward, teleports to a random location
+# 'G' : terminates
+# '$' : +100 reward, terminates
+# '@' : vortex, very hard to escape
+
+class GridWorld(MDP):
+    actions = ('n', 's', 'e', 'w')
+
+    def __init__(self, charMap, discountFactor=0.9, ssp=False,
+                     nom_prob = 0.5):
+        self.cmap = charMap
+        self.num_rows = len(charMap)
+        self.num_cols = len(charMap[0])
+        self.states = [(i, j) for i in range(self.num_cols) \
+                       for j in range(self.num_rows)] + [(-1, -1)]
+        self.q = None
+        self.discount_factor = discountFactor
+        self.start = uniform_dist(self.states)
+        self.ssp = ssp
+        self.nominal_trans_prob = nom_prob
+        # graphics stuff
+        self.ax = None
+        self.colorBar = None
+
+    def legal(self, s):
+        (i, j) = s
+        return (0 <= i < self.num_cols) and (0 <= j < self.num_rows) and \
+               self.habitable(self.getChar(i, j))
+
+    def terminal(self, s):
+        (i, j) = s
+        return i == -1 or self.getChar(i, j) in ('$', 'G')
+
+    def habitable(self, c):
+        return c != '#'
+
+    def reward_fn(self, s, a):
+        (i, j) = s
+        if i == -1:
+            # game over
+            return 0
+        elif self.ssp:
+            # Treat this as a stochastic shortest paths problem, with
+            # a reward of -1 for every step by default until termination
+            return -1
+        else:
+            return self.charReward(self.getChar(i, j))
+
+    def charReward(self, c):
+        if c == '$':
+            return 100
+        elif c == '*':
+            return 50
+        else:
+            return 0
+
+    # i and j are coordinates where origin is at bottom left
+    # i is in the traditional x direction, which is columns
+    # j is in the traditional y direction, which is rows
+    def getChar(self, i, j):
+        # cmap is [row][column]
+        # rows are flipped
+        return self.cmap[self.num_rows - j - 1][i]
+
+    # actions
+    def aOffset(self, a):
+        if a == 'n':
+            return (0, 1)
+        elif a == 's':
+            return (0, -1)
+        elif a == 'e':
+            return (1, 0)
+        else:
+            return (-1, 0)
+
+    def transition_model(self, s, a):
+        (i, j) = s
+        if self.terminal((i, j)) or not self.legal((i, j)):
+            return DDist({(-1, -1): 1.0})
+
+        if self.getChar(i, j) == '*':
+            # Teleport!
+            return uniform_dist([s for s in self.states if
+                                 self.habitable(s)])
+
+        if self.getChar(i, j) == '@':
+            # Vortex!  Stay here with high probability
+            stayProb = 0.99
+            (ni, nj) = (i, j)
+        else:
+            stayProb = self.nominal_trans_prob  # Prob we move to the nominal
+            #  next state
+            (ai, aj) = self.aOffset(a)  # Offset due to action
+            (ni, nj) = (i + ai, j + aj)  # Nominal next state
+            if not self.legal((ni, nj)):
+                # Destination not legal, stay centered here
+                (ni, nj) = (i, j)
+
+        neighbors = [nn for nn in \
+                     [(ni + 1, nj), (ni - 1, nj), (ni, nj + 1), (ni, nj - 1)] \
+                     if self.legal(nn)]
+        result = {}
+        result[(ni, nj)] = stayProb
+        for nn in neighbors:
+            result[nn] = (1 - stayProb) / len(neighbors)
+        # print('s', s, 'a', a, 'nominal', (ni, nj), 'stayProb', stayProb)
+        # print(result, sum(result.values()))
+        # input('go')
+        rdist = DDist(result)
+        rdist.normalize()
+        return rdist
+
+    def rewardMap(self):
+        if self.ax is None:
+            plt.ion()
+            plt.figure(facecolor="white")
+            self.ax = plt.subplot()
+        ima = np.array([[max(self.reward_fn((i, j), a) for a in self.actions) 
+                         for i in range(self.num_cols)] \
+                         for j in range(self.num_rows)])
+        self.im = self.ax.imshow(np.flipud(ima),
+                                 interpolation='none', cmap='viridis',
+                                 extent=[-0.5, self.num_cols - 0.5,
+                                         -0.5, self.num_rows - 0.5])
+
+    def valueMap(self):
+        if self.ax is None:
+            plt.ion()
+            plt.figure(facecolor="white")
+            self.ax = plt.subplot()
+        ima = np.array([[self.value((i, j)) \
+                         for i in range(self.num_cols)] \
+                        for j in range(self.num_rows)])
+        self.im = self.ax.imshow(np.flipud(ima),
+                                 interpolation='none', cmap='viridis',
+                                 extent=[-0.5, self.num_cols - 0.5,
+                                         -0.5, self.num_rows - 0.5])
+
+
+    def policyMap(self, newAxes = False, clim_min = None, clim_max = None,
+                  s = None):
+        if newAxes or self.ax is None:
+            plt.ion()
+            plt.figure(facecolor="white")
+            self.ax = plt.subplot()
+        else:
+            self.ax.clear()
+
+        c = {'n': 0, 's': 1, 'e': 2, 'w': 3}
+
+        def xoff(a):
+            if a == 'e':
+                return .5
+            elif a == 'w':
+                return -.5
+            else:
+                return 0
+
+        def yoff(a):
+            if a == 'n':
+                return .5
+            elif a == 's':
+                return -.5
+            else:
+                return 0
+
+        X = range(self.num_cols)
+        Y = range(self.num_rows)
+        A = [[self.greedy((i, j)) \
+              for i in range(self.num_cols)] \
+             for j in range(self.num_rows)]
+        # In yet another orientation.  Have to transpose
+        U = [[xoff(A[j][i]) for i in range(self.num_cols)] \
+             for j in range(self.num_rows)]
+        V = [[yoff(A[j][i]) for i in range(self.num_cols)] \
+             for j in range(self.num_rows)]
+        self.ax.quiver(X, Y, U, V)
+        ima = np.array([[self.value((i, j)) \
+                         for i in range(self.num_cols)] \
+                        for j in range(self.num_rows)])
+        im = self.ax.imshow(np.flipud(ima),
+                            interpolation='none', cmap='viridis',
+                            extent=[-0.5, self.num_cols - 0.5,
+                                    -0.5, self.num_rows - 0.5])
+        # Used to make figuresn
+        # im.set_clim(0, 300)
+        if clim_min is not None:
+            im.set_clim(clim_min, clim_max)
+
+        if newAxes or self.colorBar is None:
+            self.colorBar = plt.colorbar(im)
+        else:
+            self.colorBar.update_normal(im)
+            #self.colorBar.on_mappable_changed(im)
+       
+       
+
+# learn parameter is {False, 'Q', 'Dyna'}
+def test_grid(grid, discountFactor = 0.9, ssp = False,
+            learn = False, learningRate = 0.1,
+            iters = 1000, eps_vi = 0.001, eps_ql = 0.1,
+            interactive = False, learnVisUpdateRate = 500, newAxes = False,
+            nom_prob = None,
+            printFinal = False,
+            clim_min = None, clim_max = None,
+            learning_curve = False,
+            policy_map = True):
+    def interact(iter = 0, s = None, a = None, r = None):
+        if not learn or (iter % learnVisUpdateRate == 0):
+            if policy_map:
+                m.policyMap(newAxes, s = s,
+                        clim_min = clim_min, clim_max = clim_max)
+                plt.title("Step:" + str(iter))
+                if not newAxes:
+                    plt.pause(0.1)
+                    print('Step:', iter, 'Current state:', s)
+                    input('go?')
+                else:
+                    plt.savefig('plot'+str(iter))
+            if learning_curve:
+                policy_evals.append((iter, m.policy_eval()))
+
+    interactive_fn = interact if interactive else None
+    policy_evals = []
+
+    m = GridWorld(grid, discountFactor = discountFactor, ssp = ssp)
+    if nom_prob is not None:
+        m.nominal_trans_prob = nom_prob
+    if learn == 'Q':    
+        m.Q_learn(lr = learningRate, eps = eps_ql,
+                  iters = iters, interactive_fn = interactive_fn)
+    elif learn == 'Dyna':
+        m.dyna_q(eps = eps_ql,
+                 value_iteration_update_rate = 100,
+                 iters = iters, interactive_fn = interactive_fn)
+    else:
+        m.value_iteration(eps = eps_vi, interactive_fn = interactive_fn,
+                          max_iters = iters)
+
+    if printFinal:
+        for s in m.states:
+            print(s, ':', m.greedy(s), ':', m.value(s))
+    return policy_evals
+
+
+smallGrid = ['....',
+             '....',
+             '..$.',
+             '....']
+
+medGrid1 = ['..........',
+            '..........',
+            '..........',
+            '...$......',
+            '..........',
+            '..........',
+            '..........',
+            '.......$..',
+            '..........',
+            '..........']
+
+medVortex = ['..........',
+             '..........',
+             '..........',
+             '..........',
+             '..........',
+             '..........',
+             '..........',
+             '..@....$..',
+             '..........',
+             '..........']
+
+medGrid2 = ['..........',
+            '#####.###.',
+            '..........',
+            '..#####.##',
+            '.....###..',
+            '.......#..',
+            '.....###..',
+            '......#$..',
+            '......#...',
+            '..........']
+
+medStar = ['..........',
+           '........*.',
+           '..........',
+           '..........',
+           '..........',
+           '..........',
+           '..........',
+           '..........',
+           '.$........',
+           '..........']
+
+medGoal100 = ['..........',
+              '..........',
+              '..........',
+              '...$......',
+              '..........',
+              '..........',
+              '..........',
+              '..........',
+              '..........',
+              '..........']
+
+medGoal = ['..........',
+           '..........',
+           '..........',
+           '...G......',
+           '..........',
+           '..........',
+           '..........',
+           '..........',
+           '..........',
+           '..........']
+
+def foo():
+    fizz = GridWorld(smallGrid)
+    fizz.rewardMap()
+
+def test_easy_grid():
+    return test_grid(medGrid1, interactive = True, iters = 10,
+                         discountFactor = 0.9)
+
+def test_grid_walls(eps = 0.00001, discountFactor = 0.9, iters = 50):
+    return test_grid(medGrid2, eps_vi = eps, discountFactor = discountFactor,
+                     interactive = True, iters = iters)
+
+def test_grid_star(gamma = 0.9, iters = 50):
+    return test_grid(medStar, discountFactor = gamma,
+                     interactive = True, iters = iters)
+
+def test_vortex(gamma = 0.9, iters = 50, nominal_trans_prob = None):
+    return test_grid(medVortex, discountFactor = gamma,
+                    interactive = True, iters = iters,
+                   nom_prob = nominal_trans_prob)
+
+
+# Learning examples
+
+def test_small_grid_learn(ur = 1, iters = 200,
+                          clim_min = None, clim_max = None):
+    return test_grid(smallGrid, learn = True, iters = iters,
+                         eps_ql = 0.1,
+                        learnVisUpdateRate = ur, interactive = True,
+                     clim_min = clim_min, clim_max = clim_max)
+
+def test_easy_grid_learn():
+    return test_grid(medGrid1, learn = True, iters = 50000,
+                         eps_ql = 0.1,
+                        learnVisUpdateRate = 200, interactive = True)
+
+def test_grid_walls_learn():
+    return test_grid(medGrid2, learn = True, iters = 5000000,
+                         eps_ql = 0.1,
+                        learnVisUpdateRate = 10000, interactive = True)
+
+def testGridSSPLearn(iters = 100000, learn_vis_update_rate = 5000,
+                         nom_prob = 0.8, eps = 0.1, interactive = False):
+    return test_grid(medGoal, learn = True, ssp = True, iters = iters,
+                         eps_ql = eps, learningRate = 0.5, 
+                        learnVisUpdateRate = learn_vis_update_rate, newAxes = True, interactive = interactive, nom_prob = nom_prob)
+
+def testGridGoalLearn(iters = 100000, learn_vis_update_rate = 5000,
+                          nom_prob = 0.8, eps = 0.1, interactive = False):
+    return test_grid(medGoal100, learn = True, iters = iters,
+                         eps_ql = eps, learningRate = 0.5, 
+                        learnVisUpdateRate = learn_vis_update_rate, newAxes = True, interactive = interactive, nom_prob = nom_prob)
+
+def testSmallSSP(iters = 200, ur = 20):
+    return test_grid(smallGrid, learn = True, iters = iters, ssp = True,
+                        learnVisUpdateRate = ur, newAxes = True, interactive = True)
+
+def Q_vs_Dyna(grid):
+    def plotone(data, label):
+        x = np.array([d[0] for d in data])
+        means = np.array([d[1][0] for d in data])
+        sterrs = np.array([d[1][1] for d in data])
+        yl = means - sterrs
+        yu = means + sterrs
+        plt.fill_between(x, yl, yu, alpha=0.5, label = label)
+
+    q_curve = test_grid(grid, learn = 'Q', iters = 30000, interactive = True,
+                        learnVisUpdateRate = 1000, learning_curve = True,
+                        policy_map=False)
+    dyna_curve = test_grid(grid, learn = 'Dyna', iters = 30000, interactive = True,
+                        learnVisUpdateRate = 1000, learning_curve = True,
+                        policy_map=False)
+    fig, ax = plt.subplots()
+    plotone(q_curve, 'Q-learning')
+    plotone(dyna_curve, 'Dyna-Q')
+    ax.legend()
+    ax.set_xlabel('Training steps')
+    ax.set_ylabel('Average reward per step')
+    plt.show()
+    return q_curve, dyna_curve
+
+
+
+#test_easy_grid()
+#test_grid_walls()
+#test_small_grid_learn(ur = 5)
+#test_grid_star(gamma = 1, iters = 20)
+#test_grid_star(gamma = 1, iters = 20)
+#test_small_grid_learn(ur = 10, iters = 200)
+#test_grid_walls_learn()
+
---
+++ mdp.py
@@ -0,0 +1,200 @@
+from dist import uniform_dist, delta_dist, DDist, mixture_dist
+from util import argmax_with_val, argmax
+import numpy as np
+
+def rand_argmax(l, f):
+    vals = np.array([f(x) for x in l])
+    # Randomize in case of ties
+    return l[np.random.choice(np.flatnonzero(vals == vals.max()))]
+
+class MDP:
+    # states: list or set of states
+    # actions: list or set of actions
+    # transitionModel: function from (state, action) into DDist over next state
+    # rewardFn: function from (state, action) to real-valued reward
+    # discountFactor: real, greater than 0, less than or equal to 1
+    # startDist: optional instance of DDist, specifying initial state dist
+    #    if it's unspecified, we'll use a uniform over states
+
+    def __init__(self, states, actions, transition_model, reward_fn, 
+                     discount_factor = 1.0, start_dist = None):
+        self.states = states
+        self.actions = actions
+        self.transition_model = transition_model
+        self.reward_fn = reward_fn
+        self.discount_factor = discount_factor
+        self.start = start_dist if start_dist else uniform_dist(states)
+        self.q = None
+
+    # Given a state, return True if the state should be considered to
+    # be terminal.  You can think of a terminal state as generating an
+    # infinite sequence of zero reward.
+    def terminal(self, s):
+        return False
+
+    # Perform value iteration on this MDP.  Terminate when the
+    # max-norm distance between two successive value function
+    # estimates is less than eps.
+    # interactiveFn is a function of zero arguments or None;  if it is
+    # not None, it will be called once per iteration, for visuzalization
+
+    # Sets self.q to be a dictionary mapping (s, a) pairs into Q values
+    # This must be initialized before interactiveFn is called the first time.
+    
+    def value_iteration(self, eps = 0.0001, interactive_fn = None,
+                        max_iters = 10000):
+        mnd = float('inf')
+        self.value_iteration_init()
+        for it in range(max_iters):
+            if interactive_fn: interactive_fn()
+            new_q = self.value_iteration_update()
+            mnd = max_norm_dist(self.q, new_q)
+            if mnd  < eps:
+                print('Value iteration finished.  Iterations:', it, 'eps', eps)
+                return
+            self.q = new_q
+        print('Max norm dist is', mnd, 'after', max_iters, 'iters')
+
+    def value_iteration_init(self, val = 0.0):
+        self.q = dict([((s, a), val) for s in self.states for a in
+                       self.actions])
+    
+    # Sparse because we probably won't have all the (s, a, s') triples
+    def transition_model_init(self):
+        self.t_counts = dict([((s, a), {}) for s in self.states for a in
+                           self.actions])
+        
+    def transition_model_update(self, s, a, s_prime):
+        if s_prime in self.t_counts[(s, a)]:
+            self.t_counts[(s, a)][s_prime] += 1
+        else:
+            self.t_counts[(s, a)][s_prime] = 1
+
+    def approx_transition_model(self, s, a):
+        counts = self.t_counts[(s, a)]
+        total = sum(counts.values())
+        probs = dict([(s_prime, count / total) for (s_prime, count) in
+                      counts.items()])
+        return uniform_dist(self.states) if total == 0 else DDist(probs)
+
+    def reward_model_init(self):
+        self.r_counts = dict([((s, a), 0) for s in self.states for a in
+                           self.actions])
+        self.r_total = 0
+
+    def reward_model_update(self, s, a, r):
+        self.r_counts[(s, a)] += r        
+        self.r_total += 1
+
+    def value_iteration_update(self, use_true_model = True):
+        new_q = {}
+        t = self.transition_model if use_true_model else self.approx_transition_model
+        for s in self.states:
+            for a in self.actions:
+                new_q[(s, a)] = self.reward_fn(s, a) + self.discount_factor * \
+                        t(s, a).expectation(self.value)
+        return new_q
+
+    # Given a state, return the action that is greedy with reespect to the
+    # current definition of the q function
+    def greedy(self, s):
+        return rand_argmax(self.actions, lambda a: self.q[(s, a)])
+
+    # Given a state, return the value of that state, with respect to the 
+    # current definition of the q function
+    def value(self, s):
+        return max(self.q[(s, a)] for a in self.actions)
+
+    # Randomly choose a state from the initial state distribution
+    def init_state(self):
+        return self.start.draw()
+
+    # Simulate a transition from state s, given action a.  Return
+    # reward for (s,a) and new state, drawn from transition.
+
+    # If a terminal state is encountered, get next state from initial state distribution
+    def sim_transition(self, s, a):
+        return (self.reward_fn(s, a),
+                self.init_state() if self.terminal(s) else
+                    self.transition_model(s, a).draw())
+
+    # Q learning, handles terminal states
+    def Q_learn(self, lr=.1, iters=1000, interactive_fn=None, eps=0.5, verbose=False):
+        self.q = dict(
+            [((s, a), 0.0) for s in self.states for a in self.actions])
+        s = self.init_state()
+        for i in range(iters):
+            a = self.epsilon_greedy(s, eps)
+            r, s_prime = self.sim_transition(s, a)
+            future_val = 0 if self.terminal(s) else self.value(s_prime)
+            self.q[(s, a)] = (1 - lr) * self.q[(s, a)] + \
+                                 lr * (r + self.discount_factor * future_val)
+            if interactive_fn:
+                interactive_fn(i, s, a, r)
+            s = s_prime
+            if verbose:
+                print(s, a, r, s_prime, self.q[(s, a)], self.terminal(s))
+
+    def epsilon_greedy(self, s, eps=0.5):
+        # Epsilon greedy action selection
+        return mixture_dist(delta_dist(self.greedy(s)),
+                            uniform_dist(self.actions), 1 - eps).draw()
+
+    # Compute the q value of action a in state s with horizon h,
+    # using expectimax
+    def q_em(self, s, a, h):
+        if h == 0:
+            return 0
+        else:
+            return self.reward_fn(s, a) + \
+                   self.discount_factor * \
+                   sum([max(
+                       [p * self.q_em(sp, ap, h - 1) for ap in self.actions]) \
+                        for (sp, p) in self.transition_model(s, a).d.items()])
+
+    def policy_em(self, s, h):
+        return argmax(self.actions, lambda a: self.q_em(s, a, h))
+    
+    # Estimate the transition model and do value iteration sweeps
+    def dyna_q(self, iters = 1000, eps = 0.5, value_iteration_update_rate = 100,
+               interactive_fn = None):
+        self.value_iteration_init()
+        self.transition_model_init()
+        self.reward_model_init()
+        s = self.init_state()
+        for i in range(iters):
+            a = self.epsilon_greedy(s, eps)
+            r, s_prime = self.sim_transition(s, a)
+            self.transition_model_update(s, a, s_prime)
+            self.reward_model_update(s, a, r)
+            if i % value_iteration_update_rate == 0 and i > 0:
+                self.q = self.value_iteration_update(use_true_model=False)
+            if interactive_fn:
+                interactive_fn(i, s, a, r)
+            s = s_prime
+    
+    # Evaluate the policy encoded in the current Q values
+    # Allow some exploration just to get out of stuck states
+    def policy_eval(self, iters = 200, reps = 10, eps = 0.1, discount = 1.0):
+        vals = []
+        for j in range(reps):
+            s = self.init_state()
+            return_val = 0
+            h = 0
+            for i in range(iters):
+                a = self.epsilon_greedy(s, eps)
+                r, s_prime = self.sim_transition(s, a)
+                s = s_prime
+                return_val += discount ** h * r
+                h = 0 if self.terminal(s) else h + 1
+            # Should maybe be per episode if the problem is episodic
+            vals.append(return_val / iters)
+        mean = np.mean(vals)
+        stderr = np.std(vals) / np.sqrt(reps)
+        return mean, stderr
+
+def max_norm_dist(q1, q2):
+    u = list(set(q1.keys()) | set(q2.keys()))
+    return argmax_with_val(u, lambda sa: abs(q1[sa] - q2[sa]))[1]
+
+     
